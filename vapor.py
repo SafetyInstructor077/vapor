ba12add456d8f82d440d2897abbcc1c6218e67d1
AlbertVador <61166410+AlbertVador@users.noreply.github.com>
2023-11-28 10:36:51 -0500

    Update vapor.py

--- vapor.py
+++ vapor.py
@@ -61,11 +61,11 @@ def insert_form():
         db.insert_jeu(jeu["nomJeu"],jeu["description"],jeu["prix"],jeu["uScore"],jeu["date"],jeu["image"],jeu["achievements"],jeu["nomDev"],nomEditeur,plat)
         return str(db._select(f"select idJeu from jeu where nomJeu = '{jeu["nomJeu"]}'")[0][0])
 
-
 @app.route('/chercher')
 def chercher():
     keyword = request.args.get('keyword')
-    jeux = db.get_jeu_by_keyword(keyword)
+    prix = request.args.get('prix')
+    jeux = db.get_jeu_by_filter(keyword,prix)
     return render_template("list_games.html", jeux=jeux)
 
 if __name__ == "__main__":
